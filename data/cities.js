7d54266e3b04cccb99a244e3fdba063e26efa49c
Gabriel U <ullmanng162@gmail.com>
2022-10-22 10:34:27 -0400

    fix newsletter route, remove unesed data

--- data/cities.js
+++
@@ -1,9 +0,0 @@
-const cities = [
-    {id: 1, name: "Montreal", region: "QC", country: "Canada"},
-    {id: 2, name: "Quebec City", region: "QC", country: "Canada"},
-    {id: 3, name: "Ottawa", region: "ON", country: "Canada"},
-    {id: 4, name: "Toronto", region: "ON", country: "Canada"},
-    {id: 5, name: "Vancouver", region: "BC", country: "Canada"}
-]
-
-module.exports = cities
--- routes/newsletters.js
+++ routes/newsletters.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function(req, res, next) {
-    return "{ message: \"This is the newsletters route\" }"
+    var testData = { message: "This is the newsletters route" }
+    res.send(JSON.stringify(testData))
 });
 
 module.exports = router;
