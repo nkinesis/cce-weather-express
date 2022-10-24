0fbc0a4ff418d459606bb874813751c75a05c20d
Gabriel U <ullmanng162@gmail.com>
2022-10-24 09:52:37 -0400

    fix cities route boilerplate

--- routes/cities.js
+++ routes/cities.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', async function(req, res, next) {
-    return "{ message: \"This is the cities route\" }"
+router.get('/', async function (req, res, next) {
+  var testData = { message: "This is the cities route" }
+  res.send(JSON.stringify(testData))
 });
 
 module.exports = router;
