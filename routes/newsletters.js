f9e7c783499abd194732c49259255afcb7f16bff
Gabriel Ullmann <ullmanng162@gmail.com>
2023-03-16 20:16:57 -0400

    add route to newsletters

--- routes/newsletters.js
+++ routes/newsletters.js
@@ -6,4 +6,15 @@ router.get('/', async function(req, res, next) {
     res.send(JSON.stringify(testData))
 });
 
+router.post('/add', async function(req, res, next) {
+    try {
+        // TODO: call a function in the newsletter controller that adds a new record on the database
+        const successCode = 404
+        res.status(successCode).send({ message: "Add a message" })
+    } catch(e) {
+        const errorCode = 404
+        res.status(errorCode).send({ message: e })
+    }
+});
+
 module.exports = router;
